e9a96a8c142fee3f439fb06215205c02adc86a98
nazaryanenko <nazar@electricwebmarketing.com>
2023-06-05 00:40:34 +0300

    fix resolving ami

--- modules/ec2/data_sources.tf
+++ modules/ec2/data_sources.tf
@@ -1,17 +1,13 @@
-data "aws_region" "current" {}
-
-data "aws_ami" "ubuntu-ami" {
+data "aws_ami" "latest_ubuntu" {
+  owners      = ["099720109477"]
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
+}
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+output "latest_ubuntu_ami_id" {
+  value = data.aws_ami.latest_ubuntu.id
 }
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2-server" {
-  ami                    = data.aws_ami.ubuntu-ami.id
+  ami                    = data.aws_ami.latest_ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [var.vpc_security_group_id]
 
